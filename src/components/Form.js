9310295019d5629f9170983b50d221388ce87b1c
Alexandra <sandra.larrosa88@gmail.com>
2021-01-05 17:28:16 +0100

    add inputs and style

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import {StyleSheet, TextInput, View} from 'react-native';
-import color from '../utils/colors';
+import colors from '../utils/colors';
 
 export default function Form() {
   return (
-    <View>
-      <TextInput placeholder="Cantidad a pedir" />
+    <View style={styles.viewForm}>
+      <View style={styles.viewInputs}>
+        <TextInput
+          placeholder="Cantidad a pedir"
+          keyboardType="numeric"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Interés %"
+          keyboardType="numeric"
+          style={[styles.input, styles.inputPercentage]}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  viewForm: {
+    position: 'absolute',
+    bottom: -90,
+    width: '85%',
+    paddingHorizontal: 50,
+    backgroundColor: colors.PRIMARY_COLOR_DARK,
+    borderRadius: 30,
+    height: 180,
+    justifyContent: 'center',
+  },
+  viewInputs: {
+    flexDirection: 'row',
+  },
+  input: {
+    height: 50,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: colors.PRIMARY_COLOR,
+    borderRadius: 5,
+    width: '60%',
+    marginRight: 5,
+    marginLeft: -5,
+    marginBottom: 10,
+    color: 'black',
+    paddingHorizontal: 15,
+  },
+  inputPercentage: {
+    width: '40%',
+    marginLeft: 5,
+  },
+});
