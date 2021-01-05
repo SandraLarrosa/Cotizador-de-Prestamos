795483773f463a4852e85e59d543510527aa7ccc
Alexandra <sandra.larrosa88@gmail.com>
2021-01-05 12:16:14 +0100

    first style component view

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+import React from 'react';
+import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>Hola Alexandra López</Text>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.safeArea}>
+        <Text>Formulario HEAD</Text>
+      </SafeAreaView>
+      <View>
+        <Text>Resultado</Text>
+      </View>
+      <View>
+        <Text>Calcular</Text>
+      </View>
+    </>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: '#f00',
+    height: 200,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+});
